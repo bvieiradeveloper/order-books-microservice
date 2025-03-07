d69b704d2a8c944e27d4e5e545932d1544cdd05b
Bruno Heinzelmann Vieira <bruno.deveng@gmail.com>
2025-03-06 21:45:47 -0300

    Create Order

---
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/controller/EventController.java
@@ -0,0 +1,12 @@
+package br.com.microservices.orchestrated.orderservice.core.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@NoArgsConstructor
+@RequestMapping("/api/event")
+public class EventController {
+}
---
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/service/OrderService.java
@@ -0,0 +1,32 @@
+package br.com.microservices.orchestrated.orderservice.core.service;
+
+import br.com.microservices.orchestrated.orderservice.core.document.Order;
+import br.com.microservices.orchestrated.orderservice.core.dto.OrderRequest;
+import br.com.microservices.orchestrated.orderservice.core.repository.OrderRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class OrderService {
+    private static final String TRANSACTION_ID_PATTERN = "%s_%s";
+    private final OrderRepository repository;
+
+    public Order createOrder(OrderRequest orderRequest){
+        var order = Order
+                .builder()
+                .products(orderRequest.getProducts())
+                .createdAt(LocalDateTime.now())
+                .transactionId(
+                        String.format(TRANSACTION_ID_PATTERN, Instant.now().toEpochMilli(), UUID.randomUUID())
+                )
+                .build();
+        repository.save(order);
+
+        return order;
+    }
+}
