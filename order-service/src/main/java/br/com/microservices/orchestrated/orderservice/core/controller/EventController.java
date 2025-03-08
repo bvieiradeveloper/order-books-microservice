8d4a72bcab26967dc9383f09853c814e9ec61d89
Bruno Heinzelmann Vieira <bruno.deveng@gmail.com>
2025-03-07 21:18:58 -0300

    Add Create/Get event

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/controller/EventController.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/controller/EventController.java
@@ -1,12 +1,28 @@
 package br.com.microservices.orchestrated.orderservice.core.controller;
 
+import br.com.microservices.orchestrated.orderservice.core.document.Event;
+import br.com.microservices.orchestrated.orderservice.core.dto.EventFilter;
+import br.com.microservices.orchestrated.orderservice.core.service.EventService;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@NoArgsConstructor
+@AllArgsConstructor
 @RequestMapping("/api/event")
 public class EventController {
+
+    private final EventService eventService;
+
+    @GetMapping
+    public Event findByFilter(EventFilter filters){
+        return  eventService.findByFilter(filters);
+    }
+    @GetMapping("all")
+    public List<Event> findAll(){
+        return eventService.findAll();
+    }
 }
