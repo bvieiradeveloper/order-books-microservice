e38a122ae0042bfb13f68d1160e986f5d791375e
Bruno Heinzelmann Vieira <bruno.deveng@gmail.com>
2025-03-09 12:01:48 -0300

    KafkaConfig_Consumer_Producer

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/service/EventService.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/service/EventService.java
@@ -5,16 +5,27 @@ import br.com.microservices.orchestrated.orderservice.core.document.Event;
 import br.com.microservices.orchestrated.orderservice.core.dto.EventFilter;
 import br.com.microservices.orchestrated.orderservice.core.repository.EventRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static io.micrometer.common.util.StringUtils.isEmpty;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class EventService {
     private final EventRepository repository;
+
+    public void notifyEnding(final Event event) {
+        event.setOrderId(event.getOrderId());
+        event.setCreatedAt(LocalDateTime.now());
+        save(event);
+        log.info("Order {} with saga notified! transactionId: {}", event.getOrderId(), event.getTransactionId());
+    }
+
 
     public Event findByFilter(EventFilter filter){
         ValidateEmptyFilter(filter);
