73b157633ad986b741fe3bc1c8d91f2093ff6854
Bruno Heinzelmann Vieira <bruno.deveng@gmail.com>
2025-03-10 18:50:58 -0300

    fix git actions

---
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/consumer/ProductValidationConsumer.java
@@ -0,0 +1,28 @@
+package br.com.microservices.orchestrated.productvalidationservice.core.consumer;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class ProductValidationConsumer {
+
+    @KafkaListener(
+            groupId = "${spring.kafka.consumer.group-id}",
+            topics = "${spring.kafka.topic.product-validation-success}"
+    )
+    public void consumerProductValidationSuccessEvent(String payload){
+        log.info("Receiving product validation success event {} from product-validation-success topic",payload);
+    }
+
+    @KafkaListener(
+            groupId = "${spring.kafka.consumer.group-id}",
+            topics = "${spring.kafka.topic.product-validation-fail}"
+    )
+    public void consumerProductValidationFailEvent(String payload) {
+        log.info("Receiving product validation fail event {} from product-validation-fail topic",payload);
+    }
+}
---
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/producer/ProductValidationProducer.java
@@ -0,0 +1,28 @@
+package br.com.microservices.orchestrated.productvalidationservice.core.producer;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ProductValidationProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Value("${spring.kafka.topic.orchestrator}")
+    private String orchestratorTopic;
+
+    public void sendEvent(String payload){
+        try {
+            kafkaTemplate.send(orchestratorTopic, payload);
+            log.info("Send event to the topic {} with data {}", orchestratorTopic, payload);
+        } catch (Exception e) {
+            log.error("Error trying to send data to topic {} with data {}", orchestratorTopic, payload, e);
+        }
+    }
+}
