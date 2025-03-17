0783aedcf941fb09edb8a08f8457e69573ca28fb
Bruno Heinzelmann Vieira <bruno.deveng@gmail.com>
2025-03-17 19:51:04 -0300

    orchestrator/kafka_config

---
+++ orchestrator-service/src/main/java/br/com/com/microservices/orchestrated/orchestratorservice/core/dto/History.java
@@ -0,0 +1,21 @@
+package br.com.com.microservices.orchestrated.orchestratorservice.core.dto;
+
+import br.com.com.microservices.orchestrated.orchestratorservice.core.enums.ESagaStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class History {
+    private String source;
+    private ESagaStatus status;
+    private String message;
+    private LocalDateTime createdAt;
+}
+
